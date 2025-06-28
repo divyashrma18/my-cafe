7e00ee87aa76190a4c5e97025cfd2e0b236645bc
divyashrma18 <dvsharma3143@gmail.com>
2025-06-28 16:39:14 +0530

    cafe website landing page

--- dv-cafe/src/App.jsx
+++ dv-cafe/src/App.jsx
@@ -1,7 +1,12 @@
 import Hero from "./components/Hero"
-
+import News from './components/News'
+import Welovecoffee from './components/Welovecoffee'
+import Footer from './components/Footer'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ImageCarousel from "./ImageCarousel";
+import ImageCarousel from "./components/ImageCarousel";
+import Menu1 from './components/Menu1'
+import Menu2 from './components/Menu2'
+import Menu3 from './components/Menu3'
 
 function App() {
 
@@ -14,6 +19,12 @@ function App() {
           <>
             <Hero />
             <ImageCarousel />
+            <Menu1 />
+            <Menu2 />
+            <Menu3 />
+            <News />
+            <Welovecoffee />
+         <Footer />
           </>
         } />
        
